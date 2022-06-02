142772d5a02736d41952011afd4d74c53091aaf1
erawhizy <erawhiz@gmail.com>
2022-06-02 15:51:12 -0700

    my first c code

---
+++ 0x00-hello_world/0-preprocessor.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>                                                                                                                                                   
+
+                                                                                                                                                                    
+
+/**                                                                                                                                                                 
+ *
+ * * main - Entry point                                                                                                                                                
+ *
+ * *                                                                                                                                                                   
+ *
+ * *Return Alawys 0 (Success)                                                                                                                                          
+ *
+ * */                                                                                                                                                                  
+
+int main (void)                                                                                                                                                     
+
+{ 
+	return (0);                                                                                                                                                      
+
+}                                                                                                                                                                               

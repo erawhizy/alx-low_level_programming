0d6d1e709797b46d29ff5fe8ca0bb7948d7131a6
erawhizy <erawhiz@gmail.com>
2022-06-08 09:48:24 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
  * Auth: Raphael Iyere
  */
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main prints _putchar followed by a new line.
  *
@@ -11,14 +11,15 @@
 
 int main(void)
 {
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+      	_putchar('\n');
 
 	return (0);
 }

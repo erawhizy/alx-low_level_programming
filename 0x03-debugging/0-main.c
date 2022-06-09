e55cbde93efb1b3e3d0bf575d1197ef1614cbf4d
erawhizy <erawhiz@gmail.com>
2022-06-09 06:32:41 -0700

    tasko

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *
  * * main - tests function that prints if integer is positive or negative
@@ -9,8 +10,8 @@ int main(void)
 {
 	int i;
 
-	i = 98;
-	 positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-	 return (0);
+	return (0);
 }

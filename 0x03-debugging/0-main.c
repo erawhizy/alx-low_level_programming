87c315c05beb319f1e9e39c69a1df9904df3c5de
erawhizy <erawhiz@gmail.com>
2022-06-09 07:02:03 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or naga 
+tive
  * Return: 0
  */
  
@@ -13,5 +14,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }

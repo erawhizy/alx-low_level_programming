fe257e21b7dacd910d1c88e6f6fb9041ed34509a
erawhizy <erawhiz@gmail.com>
2022-06-16 11:23:19 -0700

    plate

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *strncpy -> for comparing purpose
+ *@s1: panaram2
+ *@s2: panaram2
+ *Return: intger
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int result;
+
+	strcmp(s1, s2,);
+	return (result);
+}
